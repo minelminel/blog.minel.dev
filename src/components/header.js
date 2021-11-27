ae03e36084ea7ac0968eb8c68dc56cab7b2f1d0d
Michael Lawrenson <ctrlcmdspace@gmail.com>
2021-11-27 01:04:06 -0500

    Navigate using gatsby function

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import { navigate } from "gatsby"
 
 import { Header as PrimerHeader, TextInput } from '@primer/components'
 import { SearchIcon } from "@primer/octicons-react"
@@ -14,14 +14,10 @@ const Header = ({ siteTitle }) => {
 
   const handleEnter = (event) => {
     if(event.key === `Enter`) {
-      const url = new URL(window.location.href)
       const params = new URLSearchParams({
         q: query
       })
-      console.log(url)
-      url.pathname = `/search`
-      url.search = `?${params.toString()}`
-      window.location.href = url.toString()
+      navigate(`/search?${params.toString()}`)
     }
   }
 
@@ -41,34 +37,6 @@ const Header = ({ siteTitle }) => {
     </div>
   )
 }
-
-  // <header
-  //   style={{
-  //     background: `#333`,
-  //     marginBottom: `1.45rem`,
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 960,
-  //       padding: `0.45rem 1.0875rem`,
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0 }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: `white`,
-  //           textDecoration: `none`,
-  //           fontFamily: `"SFMono-Regular", Consolas, "Roboto Mono", "Droid Sans Mono", "Liberation Mono", Menlo, Courier, monospace`
-  //         }}
-  //       >
-  //         <code>{siteTitle}:~$</code>
-  //       </Link>
-  //     </h1>
-  //   </div>
-  // </header>
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
